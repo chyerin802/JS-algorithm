6cdcdd84e9f86915834d23026ec07cff63d823df
chyerin802 <52372820+chyerin802@users.noreply.github.com>
2023-05-24 22:58:49 +0900

    [Bronze II] Title: 문자열 반복, Time: 116 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -3,17 +3,12 @@ const input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
 const N = Number(input[0])
 
-
 for(let t=1; t<=N; t++){
-    const data = input[t].split(' ')
-    const R = Number(data[0])
-    const string = data[1]
+    const [R, string] = input[t].split(' ')
     let result = ''
     
     for(let s=0; s<string.length; s++){
-        for(i=0; i<R; i++){
-            result += string[s]
-        }
+        result += string[s].repeat(R)
     }
 
     console.log(result)
