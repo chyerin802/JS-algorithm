cce16db73cd7934f03f599691e8b4fab2766de92
chyerin802 <52372820+chyerin802@users.noreply.github.com>
2023-05-24 23:13:47 +0900

    [refactor] Boj2908: 최댓값 도출 방식 변경

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -6,8 +6,4 @@ const data = input[0].split(' ')
 let num1 = Number(data[0].split('').reverse().join(''))
 let num2 = Number(data[1].split('').reverse().join(''))
 
-if(num1 > num2){
-    console.log(num1)
-}else{
-    console.log(num2)
-}
+console.log(Math.max(num1, num2))
