18913c1d6b7261b46be14c688f48e8e8370a91df
chyerin802 <52372820+chyerin802@users.noreply.github.com>
2023-05-27 13:09:05 +0900

    [refactor] Boj1316: 이전 글자, 현재 글자 설정하는 방식 변경

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.js
@@ -5,20 +5,16 @@ const N = Number(input[0])
 let count = 0;
 
 const checkWord = (word) => {
-    const alphabet = new Set();
-    let prev = '';
-    let cur = '';
+    const alphabet = new Set(word[0]);
     
-    for(let c=0; c<word.length; c++){
-        prev = cur;
-        cur = word[c];
-        if(prev === cur){
- 
-            continue
-        }else if(alphabet.has(cur)){
-            return false
-        }else{
-            alphabet.add(cur)
+    for(let c=0; c<word.length-1; c++){
+        // 이전 알파벳과 현재 알파벳이 다르다면
+        if(word[c] !== word[c+1]){
+            if(alphabet.has(word[c+1])){
+                return false
+            }
+            
+            alphabet.add(word[c+1])
         }
     }
     
