a27ee262fce40951a3ba2f39a6e2711518d3c9f9
채예린 <chyerin802@gmail.com>
2023-06-03 00:51:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 3468 ms, Memory: 20336 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const stdin = (process.platform === 'linux'? fs.readFileSync('/dev/stdin').toString() :
+`5 17`).split('\n');
+
+const input = (() => {
+    let line = 0;
+    return () => stdin[line++];
+})();
+
+
+function solution(){
+    const [N, K] = input().split(" ").map(Number);
+    const visit = Array.from({ length: 100100 }, () => 0);
+
+    function bfs(N) {
+        const queue = [];
+        queue.push([N, 0]);
+        visit[N] = 1;
+        while (queue.length) {
+        const [cur, time] = queue.shift();
+        if (cur === K) return time;
+        for (next of [cur - 1, cur + 1, cur * 2]) {
+            if (!visit[next] && next >= 0 && next <= 100000) {
+            visit[next] = 1;
+            queue.push([next, time + 1]);
+            }
+        }
+        }
+    }
+    console.log(bfs(N));
+
+}
+
+solution();
