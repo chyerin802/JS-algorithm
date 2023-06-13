1d87f6715405b4c91024ac0c3195ebb668cc7593
채예린 <chyerin802@gmail.com>
2023-06-13 21:37:49 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    var answer = []
+    
+    for(let command of commands){
+        
+        let arr = array.slice(command[0] - 1, command[1])
+        arr.sort((a, b) => a - b)
+        answer.push(arr[command[2] - 1])
+    }
+    
+    return answer;
+}
