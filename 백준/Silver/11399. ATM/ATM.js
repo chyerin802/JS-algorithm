be097ee3fd2b6b8e7f02fc68230916373b28175a
채예린 <chyerin802@gmail.com>
2023-06-20 00:50:59 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 9628 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -1,14 +1,16 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-let n = Number(input[0]); // 사람의 수
-let arr = input[1].split(' ').map(Number); // 모든 처리 시간 입력받기
-// 오름차순 정렬
-arr.sort((a, b) => a - b);
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
 
-let answer = 0;
-let summary = 0;
-for (let i = 0; i < n; i++) {
-    summary += arr[i]; // i번째 사람이 기다린 총 시간
-    answer += summary; // 지금까지 소요된 총 시간 
+const n = Number(input[0])
+const time = input[1].split(' ').map(Number).sort((a, b) => a - b) // 시간 오름차순 정렬
+
+let answer = 0 // 최종 답
+let wait = 0 // 각 사람이 기다리는 시간
+
+for(let i=0; i<n; i++){
+    // 현재 사람이 시간 time[i]
+    wait += time[i];
+    answer += wait
 }
-console.log(answer);
+
+console.log(answer)
