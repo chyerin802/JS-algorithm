a2ce996b68e31174aa659eb9f330ffb76fecd1ce
채예린 <chyerin802@gmail.com>
2023-06-20 00:56:55 +0900

    [comment] Boj11399 :  주석 변경

--- 백준/Silver/11399. ATM/ATM.js
+++ 백준/Silver/11399. ATM/ATM.js
@@ -8,9 +8,9 @@ let answer = 0 // 최종 답
 let wait = 0 // 각 사람이 기다리는 시간
 
 for(let i=0; i<n; i++){
-    // 현재 사람이 시간 time[i]
-    wait += time[i];
-    answer += wait
+    // 현재 사람이 사용하는 시간 time[i]
+    wait += time[i]; // 현재 사람이 총 기다린 시간 (이전 사람들이 기다렸던 시간의 합 + 자신의 사용 시간)
+    answer += wait // 기다린 시간의 총 합
 }
 
 console.log(answer)
