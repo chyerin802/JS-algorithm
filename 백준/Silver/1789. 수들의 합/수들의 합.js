36ab622a11535365f5b856ab21f8b44ea9117e74
채예린 <chyerin802@gmail.com>
2023-06-22 15:12:49 +0900

    [Silver V] Title: 수들의 합, Time: 180 ms, Memory: 10536 KB -BaekjoonHub

---
+++ 백준/Silver/1789. 수들의 합/수들의 합.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const s = Number(input[0])
+let n = 1;
+
+while(true){
+    let sum = n * (n + 1) / 2
+    if( s === sum || s <= sum + n){
+        console.log(n)
+        break
+    }
+    n++
+}
